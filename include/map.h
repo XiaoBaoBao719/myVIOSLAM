d879f35f002d36b6c87f195f50bb4b9501700dad
Xiao-Bao Bao <xbbao@MetisTechSolution.onmicrosoft.com>
2023-01-29 12:32:01 -0800

    Created member functions and fields for Map class

--- include/map.h
+++ include/map.h
@@ -18,6 +18,46 @@ public:
     Map() {}
 
     void InsertKeyFrame(Frame::Ptr frame);
+
+    void InsertMapPoint(MapPoint::Ptr mappoint);
+
+    LandmarksType GetAllMapPoints() {
+        std::unique_lock<std::mutex> lock(data_mutex_);
+        return landmarks_;
+    }
+
+    KeyFramesType GetAllKeyFrames() {
+        std::unique_lock<std::mutex> lock(data_mutex_);
+        return keyframes_;
+    }
+
+    LandmarksType GetActiveMapPoints() {
+        std::unique_lock<std::mutex> lock(data_mutex_);
+        return active_landmarks_;
+    }
+
+    KeyFramesType GetActiceKeyFrames() {
+        std::unique_lock<std::mutex> lock(data_mutex_);
+        return active_keyframes_;
+    }
+
+    void ClearMap();
+
+private:
+    // member functions
+    void RemoveOldKeyFrames();
+
+    // member data fields
+    std::mutex data_mutux_;
+    LandmarksType landmarks_;
+    LandmarksType active_landmarks_;
+    KeyFramesType keyframes_;
+    KeyFramesType active_keyframes_;
+
+    Frame::Ptr current_frame_ = nullptr;
+
+    // settings
+    int num_active_keyframes_ = 7;          // number of keyframes in the sliding window
 };
 
 }
